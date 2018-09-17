9a5bcbc55467cf0852b3906270c3fc71d692304f
noah <noah.kennedy.professional@gmail.com>
2018-09-16 21:06:22 -0500

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ fn main() {
     let mut i2c = i2c::I2c::new().expect("Failed to create i2c");
     i2c.set_slave_address(13).expect("Failed to set slave address");
     let output = [1, 2, 3, 4, 5];
-    i2c.write(&output).expect("Failed to write data");
-    std::thread::sleep_ms(200);
-    let mut input:  [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    i2c.read(& mut input).expect("Failed to read data");
+    
+    let mut input = [0, 0, 0, 0, 0];
+    i2c.write_read(&output, &mut input).expect("Failed to write/read");
     
     for c in input.iter() {
         println!("{}", c);
