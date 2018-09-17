1a89e26da35b8841346b99272dd24d0e2fe4f7c7
noah <noah.kennedy.professional@gmail.com>
2018-09-16 20:36:02 -0500

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut i2c = i2c::I2c::new().expect("Failed to create i2c");
     i2c.set_slave_address(7);
     let output = [1, 2, 3, 4, 5];
-    i2c.write(&output);
+    i2c.write(&output).expect("Failed to write data");
     std::thread::sleep_ms(200);
     let mut input:  [u8; 5] = [0, 0, 0, 0, 0];
     i2c.read(& mut input).expect("Failed to read data");
