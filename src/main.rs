928b7e2b8097c17d5fdee7d95f04bce71b2fbf01
noah <noah.kennedy.professional@gmail.com>
2018-09-20 13:31:21 -0500

    Debugging

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
 extern crate rppal;
 use rppal::i2c;
 
-const LENGTH: usize = 1;
-
 fn main() {
     let mut i2c = Box::new(i2c::I2c::new().expect("Failed to open I2C"));
     i2c.set_slave_address(17).expect("Failed to set the slave address");
     
     loop {
-        let mut receiving_payload = [0; 5];
-        
-        let num_bytes = i2c.read(& mut receiving_payload).expect("Failed to read bytes");
-        
-        println!("{}", num_bytes);
-        
-        println!("{:?}", receiving_payload)
+        let rec = i2c.smbus_receive_byte().expect("Failed to receive byte");
+        println!("{:?}", rec)
     }
 }
